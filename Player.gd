360d13c32635a5d3d878f5f53d3e353161756741
KozinOleg97 <olegkosin.dev@yandex.ru>
2022-08-09 20:41:25 +0300

    rpg

---
+++ Player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+
+var curHp : int = 10
+var maxHp : int = 10
+var moveSpeed : int = 250
+var damage : int = 1
+var gold : int = 0
+var curLevel : int = 0
+var curXp : int = 0
+var xpToNextLevel : int = 50
+var xpToLevelIncreaseRate : float = 1.2
+var interactDist : int = 70
+var vel = Vector2()
+var facingDir = Vector2()
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process (delta):
+  
+	vel = Vector2()
+  
+	# inputs
+	if Input.is_action_pressed("move_up"):
+		vel.y -= 1
+		facingDir = Vector2(0, -1)
+	if Input.is_action_pressed("move_down"):
+		vel.y += 1
+		facingDir = Vector2(0, 1)
+	if Input.is_action_pressed("move_left"):
+		vel.x -= 1
+		facingDir = Vector2(-1, 0)
+	if Input.is_action_pressed("move_right"):
+		vel.x += 1
+		facingDir = Vector2(1, 0)
+  
+	# normalize the velocity to prevent faster diagonal movement
+	vel = vel.normalized()
+  
+	# move the player
+	move_and_slide(vel * moveSpeed, Vector2.ZERO)
